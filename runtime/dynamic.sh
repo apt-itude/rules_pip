5b3530ef14d15b7df2b409a33e9deb2ef4414d93
Travis Cline <travis.cline@gmail.com>
2018-12-15 17:41:59 -0800

    dynamic: default to py3

--- runtime/dynamic.sh
+++ runtime/dynamic.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-
 firstline=$(head -n 1 "$1")
 
 if [[ "$firstline" =~ ^#!(.*python.*)$ ]]; then
     exec ${BASH_REMATCH[1]} "$@"
 else
-    exec /usr/bin/env python "$@"
+    exec /usr/bin/env python3 "$@"
 fi

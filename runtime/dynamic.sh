cf81f38abcca0ef1097d5b3f9c8e4dbcdc3749f4
Travis Cline <travis.cline@gmail.com>
2019-01-05 23:01:24 -0600

    runtime: convert to posix compatible

--- runtime/dynamic.sh
+++ runtime/dynamic.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
+# dynamic entrypoint
+# in a posix-compatible way attempt to resolve interpreter path.
+# checks:
+# - shebang line (requires expr)
+# - path to python3
+# - path to python
 read -r firstline < "$1"
-e=$(expr "$firstline" : '#!\(.*python.*\)')
-if test -n "${e}"; then
-    exec $e "$@"
+exprpath=$(command -v expr)
+if test -n "${exprpath}"; then
+    e=$(expr "$firstline" : '#!\(.*python.*\)')
+    if test -n "${e}"; then
+        exec $e "$@"
+    fi
+fi
+py=$(command -v python3)
+if test -n "${py}"; then
+    exec $py "$@"
 else
-    exec /usr/bin/env python3 "$@"
+    exec $(command -v python) "$@"
 fi

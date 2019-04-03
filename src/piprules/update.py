9b7eb9e02270ed999ca49921757f5cbc3f6f1ece
Alex Thompson <aptbosox@gmail.com>
2019-04-03 15:37:20 -0400

    Made lock file use canonical names for packages

--- src/piprules/update.py
+++ src/piprules/update.py
@@ -8,17 +8,19 @@ LOG = logging.getLogger(__name__)
 
 def update_lock_file(lock_file, requirements):
     for requirement in requirements:
-        LOG.debug("Updating entry in lock file for %s", requirement.name)
+        canon_requirement_name = pipcompat.canonicalize_name(requirement.name)
+        LOG.debug("Updating entry in lock file for %s", canon_requirement_name)
 
         abstract_dist = pipcompat.make_abstract_dist(requirement)
         dist = abstract_dist.dist()
 
-        locked_requirement = lock_file.get_requirement(dist.project_name)
+        locked_requirement = lock_file.get_requirement(canon_requirement_name)
         locked_requirement.version = dist.version
         locked_requirement.is_direct = requirement.is_direct
 
         for dep in dist.requires():
-            locked_dep = locked_requirement.get_dependency(dep.name)
+            canon_dep_name = pipcompat.canonicalize_name(dep.name)
+            locked_dep = locked_requirement.get_dependency(canon_dep_name)
 
         link = requirement.link
         source = locked_requirement.get_source(link.url_without_fragment)

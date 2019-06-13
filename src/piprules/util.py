78763992553d86a438db870f7a1590e93a85d2bd
Alex Thompson <aptbosox@gmail.com>
2019-06-13 12:46:19 -0400

    Removed old piprules source

--- src/piprules/util.py
+++ src/piprules/util.py
@@ -1,12 +1,7 @@
-import contextlib
 import errno
 import hashlib
 import itertools
 import os
-
-
-def normalize_distribution_name(name):
-    return name.lower().replace("-", "_")
 
 
 def get_path_stem(path):
@@ -19,25 +14,6 @@ def ensure_directory_exists(path):
     except OSError as err:
         if err.errno != errno.EEXIST:
             raise err
-
-
-def get_import_path_of_module(module):
-    return os.path.abspath(os.path.dirname(os.path.dirname(module.__file__)))
-
-
-@contextlib.contextmanager
-def prepend_to_pythonpath(paths):
-    original_pythonpath = os.environ.get("PYTHONPATH")
-    original_parts = original_pythonpath.split(":") if original_pythonpath else []
-    os.environ["PYTHONPATH"] = ":".join(paths + original_parts)
-
-    try:
-        yield
-    finally:
-        if original_pythonpath is None:
-            del os.environ["PYTHONPATH"]
-        else:
-            os.environ["PYTHONPATH"] = original_pythonpath
 
 
 def full_groupby(iterable, key=None):

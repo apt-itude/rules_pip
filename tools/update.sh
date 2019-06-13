982bb020fc2825738bed8735030dc21c6b4e1232
Alex Thompson <aptbosox@gmail.com>
2019-06-13 12:34:53 -0400

    Removed old compiled tools

--- tools/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bazel build //src/bin:compile_pip_requirements.par
-bazel build //src/bin:create_pip_repository.par
-
-WORKSPACE=$(bazel info workspace)
-cp $WORKSPACE/bazel-bin/src/bin/compile_pip_requirements.par $WORKSPACE/tools/compile_pip_requirements.par
-cp $WORKSPACE/bazel-bin/src/bin/create_pip_repository.par $WORKSPACE/tools/create_pip_repository.par

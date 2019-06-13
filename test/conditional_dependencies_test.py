641bb4d168556454bdff547983ed1b1128fa55b4
Alex Thompson <aptbosox@gmail.com>
2019-06-13 11:18:12 -0400

    Moved pathlib_test into conditional_dependencies_test and removed the former

--- test/conditional_dependencies_test.py
+++ test/conditional_dependencies_test.py
@@ -1,5 +1,18 @@
 """
+This tests conditional dependencies based on Python runtime.
+
 In Python 2, isort requires futures and backports-functools-lru-cache, but in Python 3
-it has no dependencies. This import should succeed in both cases.
+it has no dependencies. The import should succeed in both cases.
+
+The pathlib2 module is only included if running Python 2.
 """
+import sys
+
 import isort
+
+if sys.version_info.major == 2:
+    import pathlib2 as pathlib
+else:
+    import pathlib
+
+assert pathlib.Path("some/test/file.txt").stem == "file"
--- test/dependency_deduplication_test.py
+++
@@ -1,7 +0,0 @@
-"""
-In Python 2, isort requires futures, but pkg_resources produces 2 different requirement
-instances for that library. This ensures that the duplicate requirements don't cause
-duplicate dependencies to be added to the build rule, which is an error.
-"""
-def test_isort():
-    import isort

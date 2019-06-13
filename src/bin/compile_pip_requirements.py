aa4458cee80d52704c45161a722de74588547374
Alex Thompson <aptbosox@gmail.com>
2019-06-13 12:37:49 -0400

    Removed old binaries

--- src/bin/compile_pip_requirements.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-"""Wrapper for the pip-tools compile entry point"""
-
-from piptools.scripts import compile as pip_compile
-
-
-if __name__ == '__main__':
-    pip_compile.cli()
